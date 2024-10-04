06d832bc074ecaee728eeb5cb6ff74bb9bc653c8
FeeNixPYK <feenix.pyk@icloud.com>
2024-10-04 01:30:20 -0400

    Populated Grid

--- src/project_1/Wordsearch.java
+++ src/project_1/Wordsearch.java
@@ -17,12 +17,23 @@ public class Wordsearch {
 		
 		char[] vowels = {'A','E','I','O','U'};
 		char[] consonants = {'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'};
+		char[][] grid = new char[row][col];
 		
+		//populate the grid
 		for(int i=0; i<row;i++) {
 			for (int j=0; j<col; j++) {
 				double chance = prob.nextDouble();
-			}
+				if (chance > 0.3) grid[i][j] = consonants[prob.nextInt(consonants.length)];
+				grid[i][j] = vowels[prob.nextInt(vowels.length)];
 		}
 	}
+		System.out.println ("Generated Letter grid");
+		for (char[] r : grid) {
+			for (char c : r) {
+				System.out.print( c +" ");
+			}
+			System.out.println();
+		}
 
+}
 }
